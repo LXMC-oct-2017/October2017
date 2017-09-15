5daa46f01cd4bb11866aadb8933bf3b41b7314d6
unknown <shogo.kataoka@luxa.local>
2017-09-15 12:28:31 +0900

    change folder configurations modify index.php add column ITEM_NAME to ITEM

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
+<?php
+    session_start();
+    if( !isset($_SESSION['LXMC_TEAM']) ){
+		$login_url = './login/login.php';
+		header("Location: $login_url");
+        exit;
+    }
+?>
 <html>
 <head>
   <title>lxmc home</title>
   <link rel="stylesheet" type="text/css" href="css/common.css">
   <link rel="stylesheet" type="text/css" href="css/home.css">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </head>
 <body>
+  <div id="item-use-history">
+	<table id="item-use-history-table">
+	</table>
+  </div>
+
   <div id="header">
     <img src="img/luxa_header.png" width="100%"/>
   </div>
@@ -20,5 +34,55 @@
   <div id="footer">
     <img src="img/luxa_footer.png" width="100%"/>
   </div>
+
+  <script type="text/javascript">
+	$('body').ready(function(){
+		getItemUseHistory();
+	});
+
+	let getItemUseHistory = function(teamId){
+		$.ajax({
+			type: "GET",
+			url: "./api/item-use-history.php",
+			dataType: "json",
+			success: showJson,
+			error: function( data ){console.log(data.responseText);},
+		});
+	}
+	
+	let createTable = function(){
+		let table = $('<table></table>').attr('id', 'item-use-history').appendTo('#item-use-history');
+		let tr = $('<tr></tr>').appendTo($('#item-use-history-table'));
+		tr.append('<th>アイテム</th>');
+		tr.append('<th>ディールID</th>');
+		tr.append('<th>ITEM_USE_RESULT</th>');
+	}
+
+	let showJson = function(json){
+		if(json.length == 0 ){
+			return;
+		}
+		createTable();
+		json.forEach( function(data){
+			let teamId = data.teamId;
+			let itemId = data.itemId;
+			let dealIds = data.dealIds;
+			let itemUseResult = data.itemUseResult;
+			let tr = $('<tr></tr>').appendTo($('#item-use-history-table'));
+			tr.append('<td>'+itemId+'</td>');
+			tr.append('<td>'+dealIds+'</td>');
+			tr.append('<td>'+itemUseResult+'</td>');
+		});
+	}
+
+	let cleanTable = function(){
+		$('#item-use-history-table').empty();
+		let tr = $("<tr></tr>").appendTo($('#item-use-history-table'));
+		tr.append('<th>TEAM_ID</th>');
+		tr.append('<th>ITEM_ID</th>');
+		tr.append('<th>DEAL_ID_LIST</th>');
+		tr.append('<th>ITEM_USE_RESULT</th>');
+	}
+  </script>
 </body>
 </html>
---
+++ login/login-auth.php
@@ -0,0 +1,43 @@
+<?php
+    session_start();
+    
+    $team_id = $_GET['team-id'];
+    $passwd_hash = hash('md5', $_GET['password']);
+    $correct_pw = selectCorrectPassword($team_id);
+    
+    if(in_array($passwd_hash, $correct_pw)){
+        //  loggin succeeded
+        login();
+    }else{
+        //  login failed
+        loginFail();
+    }
+
+    /**
+     *  select password
+     */
+    function selectCorrectPassword($team_id){
+        require './api/database/database.php';
+        $db = Database::connect();
+        $stmt = $db->query("select PASSWORD from TEAM where TEAM_ID = $team_id");
+        return $stmt->fetchAll(PDO::FETCH_COLUMN, 'PASSWORD');
+    }
+
+    /**
+     *  on succeeded
+     *  redirect to index.php and save team id
+     */
+    function login(){
+        $_SESSION['LXMC_TEAM'] = $_GET['team-id'];
+    }
+
+    /**
+     *  on failed to login
+     *  redirect to login page with parameter 'result=failed'
+     */
+    function loginFail(){
+        $url = 'login.php?result=failed';
+        header("Location: {$url}");
+        exit;
+    }
+?>
