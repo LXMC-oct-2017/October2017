a0f240b3242c12824bb1c757215d89e5cd2bb793
山口新 <arata.yamaguchi@luxa.co.jp>
2017-09-26 01:13:08 +0900

    login.php modify layout

--- login/login.php
+++ login/login.php
@@ -28,30 +28,36 @@
     </div>
     <div id="contents-inner">
       <form id="password-form" method="GET" action="./login-auth.php">
-        <select name="team-id">
-          <option value= "0">TEAM 01</option>
-          <option value= "1">TEAM 02</option>
-          <option value= "2">TEAM 03</option>
-          <option value= "3">TEAM 04</option>
-          <option value= "4">TEAM 05</option>
-          <option value= "5">TEAM 06</option>
-          <option value= "6">TEAM 07</option>
-          <option value= "7">TEAM 08</option>
-          <option value= "8">TEAM 09</option>
-          <option value= "9">TEAM 10</option>
-          <option value="10">TEAM 11</option>
-          <option value="11">TEAM 12</option>
-          <option value="12">TEAM 13</option>
-          <option value="13">TEAM 14</option>
-          <option value="14">TEAM 15</option>
-          <option value="15">TEAM 16</option>
-          <option value="16">TEAM 17</option>
-          <option value="17">TEAM 18</option>
-          <option value="18">TEAM 19</option>
-          <option value="19">TEAM 20</option>
-        </select>
-        <input type="password" name="password">
-        <input type="button" name="password-submit" value="ログイン">
+        <div id="select">
+          <select name="team-id">
+            <option value= "0">TEAM 01</option>
+            <option value= "1">TEAM 02</option>
+            <option value= "2">TEAM 03</option>
+            <option value= "3">TEAM 04</option>
+            <option value= "4">TEAM 05</option>
+            <option value= "5">TEAM 06</option>
+            <option value= "6">TEAM 07</option>
+            <option value= "7">TEAM 08</option>
+            <option value= "8">TEAM 09</option>
+            <option value= "9">TEAM 10</option>
+            <option value="10">TEAM 11</option>
+            <option value="11">TEAM 12</option>
+            <option value="12">TEAM 13</option>
+            <option value="13">TEAM 14</option>
+            <option value="14">TEAM 15</option>
+            <option value="15">TEAM 16</option>
+            <option value="16">TEAM 17</option>
+            <option value="17">TEAM 18</option>
+            <option value="18">TEAM 19</option>
+            <option value="19">TEAM 20</option>
+          </select>
+        </div>
+        <div id="password">
+          <input type="password" name="password">
+        </div>
+        <div id="button">
+          <input type="button" name="password-submit" value="ログイン">
+        </div>
       </form>
     </div>
   </div>
