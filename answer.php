5a267d29954718d92db13982416d0c5d74f0c16a
luxa-yamaguchi <arata.yamaguchi@luxa.co.jp>
2017-10-02 22:49:39 +0900

    modify answer.php layout

--- answer.php
+++ answer.php
@@ -5,6 +5,8 @@
   <title>lxmc answer</title>
   <link rel="stylesheet" type="text/css" href="css/common.css">
   <link rel="stylesheet" type="text/css" href="css/home.css">
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="js/LxmcApi.js"></script>
 </head>
 <body>
   <div id="header">
@@ -16,15 +18,60 @@
       選択されたディールの合計金額を公開します
     　</p>
     </div>
-
-    <!-- ディール一覧 -->
-
-    <div class="link">
-      <a href="result.php">合計金額公開</a>
-    </div>
+    <button class="switch">ディール一覧</button>
+    <button class="submit">合計金額公開</button>
   </div>
   <div id="footer">
     <img src="img/luxa_footer.png" width="100%"/>
   </div>
+
+<script>
+$('.switch').click(function() {
+  if ($('.deals').length) {
+    $(".deals").remove();
+  } else {
+	  let onSucceeded = function(json) {
+        var list = document.createElement('div');
+        list.className = 'deals';
+
+        json.forEach(function(val, key) {
+          var $checkbox = $('<input></input>', {
+            name: "checkbox-group",
+            type: "checkbox",
+            value: json[key].dealId,
+          });
+
+          var $dealTitle = $('<p></p>', {
+            "class": "deal-title",
+            html: json[key].dealTitle
+          });
+
+          var $deal = $('<div>').addClass('deal').append($checkbox).wrapInner($dealTitle);
+
+          list.appendChild($deal[0]);
+        });
+        $('.submit').before(list);
+      };
+
+	  let api = new LxmcApi();
+	  api.errorHandler = function(data){console.log(data);};
+	  api.callApi('api/get-deal-all.php', onSucceeded );
+  }
+});
+
+$('.submit').click(function() {
+  deal = $('[name="radio-group"]:checked').val();
+  console.log(deal);
+
+  let api = new LxmcApi();
+  api.data = {'dealId': 100000};
+  api.errorHandler = function(XMLHttpRequest, textStatus, errorThrown) {
+	console.log(XMLHttpRequest.status);
+	console.log(textStatus);
+	console.log(errorThrown.message);
+  }
+  api.callApi('api/use-item1.php', function(data){console.log(data);});
+});
+</script>
 </body>
 </html>
